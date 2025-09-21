1f90b711575c8622ac9524f1e324c8746f85d747
Yana Kalsina <yaprogrammer18@gmail.com>
2025-09-21 13:41:25 +0300

    Added hw for 3rd lesson about compilation

---
+++ src/Compilation_process/zeros_in_massive.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    // пусть массив задан в программе
+    int list[] = { 0, 1, 3, 0, 6, 0, 7, 4, 6, 2, 0, 0 };
+    int count = 0;
+    size_t length = sizeof(list) / sizeof(int);
+
+    for (int i = 0; i < length; i++) {
+        if (list[i] == 0) {
+            count += 1;
+        }
+    }
+    printf("%d", count);
+    return 0;
+}
