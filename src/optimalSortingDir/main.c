8150c1841486a803b5651ef3bf89f12c5991a337
Yana Kalsina <yaprogrammer18@gmail.com>
2025-10-06 19:36:35 +0300

    Remove some silly things from files

--- src/optimalSortingDir/main.c
+++ src/optimalSortingDir/main.c
@@ -2,40 +2,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
+int main() {
 
-    int* onlyNumbersList = calloc(200, sizeof(int));
-    int length = 0;
-    char nextSymbol = 0;
-    while (nextSymbol != '\n') {
-        int inputNumber = 0;
-        int input = scanf("%d", &inputNumber);
-        if (input == 1) // scanf успешно прочитал введенное число
-        {
-            onlyNumbersList[length++] = inputNumber; // добавляем число в список
-        }
-
-        else {
-            return 1;
-        }
-
-        nextSymbol = getchar(); // читаем символ, который был введен дальше
-        if (nextSymbol == '\n') {
-            break;
-        }
+  int *onlyNumbersList = calloc(200, sizeof(int));
+  int length = 0;
+  char nextSymbol = 0;
+  while (nextSymbol != '\n') {
+    int inputNumber = 0;
+    int input = scanf("%d", &inputNumber);
+    if (input == 1) // scanf успешно прочитал введенное число
+    {
+      onlyNumbersList[length++] = inputNumber; // добавляем число в список
     }
 
-    // for (int i = 0; i<length; i++)
-    // {
-    //     printf("%d ", onlyNumbersList[i]);
-    // }
+    else {
+      return 1;
+    }
 
-    printf("%d\n", sort(onlyNumbersList, length));
+    nextSymbol = getchar(); // читаем символ, который был введен дальше
+    if (nextSymbol == '\n') {
+      break;
+    }
+  }
 
-    return 0;
+  printf("%d\n", sort(onlyNumbersList, length));
+
+  return 0;
 }
-
 
 // пример: ввод 1 2 3 4
 // должно вернуть : 4, так как каждая цифра изменила свою позицию
