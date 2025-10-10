70fba14dc716e5951d6a1098761462b5a9f871ed
Yana Kalsina <yaprogrammer18@gmail.com>
2025-10-10 21:41:50 +0300

    Change list into array in sort.h and sort.c

--- src/optimalSortingDir/sort.c
+++ src/optimalSortingDir/sort.c
@@ -1,23 +1,23 @@
 #include "sort.h"
 #include <stdlib.h>
 
-int sort(int *list, int length) {
+int sort(int *array, int length) {
 
   int *unsortedArray =
       calloc(length, sizeof(int)); // c помощью этого списка будем счиатать
                                    // элементы, изменившие свою позицию
   for (int i = 0; i < length; i++) {
-    unsortedArray[i] = list[i];
+    unsortedArray[i] = array[i];
   }
   // копируем переданный список
 
   // сортировка пузырьком
   for (int i = 0; i < length; i++) {
     for (int j = 0; j < length - 1 - i; j++) {
-      if (list[j] > list[j + 1]) {
-        int tmp = list[j];
-        list[j] = list[j + 1];
-        list[j + 1] = tmp;
+      if (array[j] > array[j + 1]) {
+        int tmp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = tmp;
       }
     }
   }
@@ -25,7 +25,7 @@ int sort(int *list, int length) {
   int count = 0;
   // смотрим какие элементы изменили свою позицию
   for (int k = 0; k < length; k++) {
-    if (list[k] != unsortedArray[k]) {
+    if (array[k] != unsortedArray[k]) {
       count += 1;
     }
   }
