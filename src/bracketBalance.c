d2a3a7461c2d26c21ba59efae02e639318910c4e
Yana Kalsina <yaprogrammer18@gmail.com>
2025-10-12 00:40:15 +0300

    Fix bracket balance. Now you can put as many symbols as you want

--- src/bracketBalance.c
+++ src/bracketBalance.c
@@ -2,42 +2,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-bool checkBrackets(char* stringWithBrackets)
-{
-    //специальный счетчик, "(" это +1 ")" это -1. Если скобочная посл-ть правильная, counter == 0
-    int counter = 0;
-    // переменная, чтобы сказать пользователю, что нет скобок вообще или что скобочная последовательность неверная
-    bool bracketsExist = false;
-    for (int i = 0; i < 100; i++) {
-        if (stringWithBrackets[i] == '(') {
-            counter++;
-            bracketsExist = true;
-        }
-        if (stringWithBrackets[i] == ')') {
-            counter--;
-            bracketsExist = true;
-        }
-        if (counter < 0) {
-            break;
-        }
+bool checkBrackets(char *stringWithBrackets, int len) {
+  // специальный счетчик, "(" это +1 ")" это -1. Если скобочная посл-ть
+  // правильная, counter == 0
+  int counter = 0;
+  // переменная, чтобы сказать пользователю, что нет скобок вообще или что
+  // скобочная последовательность неверная
+  bool bracketsExist = false;
+  for (int i = 0; i < len; i++) {
+    if (stringWithBrackets[i] == '(') {
+      counter++;
+      bracketsExist = true;
     }
-    if (bracketsExist && counter == 0) {
-        return true;
+    if (stringWithBrackets[i] == ')') {
+      counter--;
+      bracketsExist = true;
     }
-    return false;
+    if (counter < 0) {
+      break;
+    }
+  }
+  if (bracketsExist && counter == 0) {
+    return true;
+  }
+  return false;
 }
 
-int main()
-{
-    char* stringWithBrackets = calloc(100, sizeof(char));
-    scanf("%100[^\n]", stringWithBrackets);
-    bool isCorrect = checkBrackets(stringWithBrackets);
-
-    if (isCorrect) {
-        printf("Скобочная последовательность правильная\n");
-    } else {
-        printf("Скобочная последовательность неправильная или не было введено ни одной скобки\n");
-    }
-    free(stringWithBrackets);
-    return 0;
+int main() {
+  int len = 0;
+  printf("Введите, сколько символов Вы хотите ввести, закончите ввод, нажав "
+         "enter\n");
+  scanf("%d", &len);
+  char *stringWithBrackets = malloc(len * sizeof(char));
+  getchar();
+  for (int i = 0; i < len; i++) {
+    scanf("%c", &stringWithBrackets[i]);
+  }
+  bool isCorrect = checkBrackets(stringWithBrackets, len);
+  if (isCorrect) {
+    printf("Скобочная последовательность правильная\n");
+  } else {
+    printf("Скобочная последовательность неправильная или не было введено ни "
+           "одной скобки\n");
+  }
+  free(stringWithBrackets);
+  return 0;
 }
