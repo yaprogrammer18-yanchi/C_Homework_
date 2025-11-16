a66db8025486ec54b2072ace3599b96304f51c91
Yana Kalsina <yaprogrammer18@gmail.com>
2025-11-16 19:35:20 +0300

    Changed the solving

--- src/listLesson/sortedListTask.c
+++ src/listLesson/sortedListTask.c
@@ -2,54 +2,99 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-// подразумевается что пользователь не вводит строки
 
-int main() {
-  printf("Вводите команды. После каждой введенной команды нажимайте Enter\n");
-  printf("Чтобы завершить работу с программой, введите 0\n");
-  int tmp = 0;
-  List *list = newList();
-  int element = 0;
-  int index = 0;
-  while (1) {
-    scanf("%d", &tmp);
-    if (tmp == 0) {
-      deleteList(list);
-      break;
-    }
-    if (tmp == 1) {
-      printf("Введите значение, которое хотите добавить, после того, как "
-             "введете, нажмите Enter\n");
-      scanf("%d", &element);
-      insert_sorted_in_ascending_order(list, element);
-      printf("Элемент успешно добавлен\n");
-      printf("Введите команду:\n");
-    }
-    if (tmp == 2) {
-      printf("Введите индекс элемента, который хотите удалить, после того, как "
-             "введете нажмите Enter\n");
-      scanf("%d", &index);
-      if (removeElement(list, index)) {
-        printf("Элемент успешно удален.\n");
-        printf("Введите команду:\n");
-      } else {
-        printf("Что-то пошло не так. Возможно введен неправильный индекс.\n");
-        printf("Введите команду:\n");
-      }
-    }
-    if (tmp == 3) {
-      printf("Список элементов:\n");
-      printList(list);
-      printf("Введите команду:\n");
-    } else if (tmp != 1 && tmp != 2 && tmp != 3) {
-      printf("Вероятно, такой команды не существует.\n");
-      printf("Введите команду:\n");
-    }
-  }
-  printf("Работа с программой завершена. Память очищена.\n");
-  return 0;
+void printMenu()
+{
+    printf("\n=== МЕНЮ КОМАНД ===\n");
+    printf("1 - Добавить элемент (с сортировкой по возрастанию)\n");
+    printf("2 - Удалить элемент по индексу\n");
+    printf("3 - Вывести список\n");
+    printf("0 - Выход из программы\n");
 }
 
-// P.S. Не очень понимаю, почему моя программа так себя ведет, если ввести
-// строки, причем не важно в какой момент...
-// + я же даже проверку написала на то, что элемент может быть не интовым...
+void clearInputBuffer()
+{
+    int c;
+    while ((c = getchar()) != '\n') { }
+}
+
+void addElementInList(List* list)
+{
+    int element = 0;
+    printf("Введите элемент который хотите добавить\n");
+
+    if (scanf("%d", &element) != 1) {
+        printf("Ошибка: введено некорректное значение!\n");
+        clearInputBuffer();
+        return;
+    }
+    clearInputBuffer();
+    insert_sorted_in_ascending_order(list, element);
+    printf("Элемент успешно добавлен\n");
+}
+
+void removeElemetFromList(List* list)
+{
+    int index = 0;
+    printf("Введите индекс элемента, который хотите удалить: ");
+    if (scanf("%d", &index) != 1) {
+        printf("Ошибка: введен некорректный индекс!\n");
+        clearInputBuffer();
+        return;
+    }
+    clearInputBuffer();
+
+    if (removeElement(list, index)) {
+        printf("Элемент успешно удален.\n");
+    } else {
+        printf("Что-то пошло не так. Возможно введен неправильный индекс.\n");
+    }
+}
+
+void printTheList(List* list)
+{
+    printf("Список элементов:\n");
+    printList(list);
+}
+
+void handleCommands()
+{
+    printf("Вводите команды. После каждой введенной команды нажимайте Enter\n");
+    printf("Чтобы завершить работу с программой, введите 0\n\n");
+    List* list = newList();
+    int command = 0;
+    while (1) {
+        printf("Введите команду: ");
+        if (scanf("%d", &command) != 1) {
+            printf("Ошибка: пожалуйста, введите число от 0 до 3!\n");
+            clearInputBuffer();
+            continue; // Пропускаем остаток цикла и начинаем заново
+        }
+        clearInputBuffer();
+        if (command == 0) {
+            break;
+        }
+        switch (command) {
+        case 1:
+            addElementInList(list);
+            break;
+        case 2:
+            removeElemetFromList(list);
+            break;
+        case 3:
+            printTheList(list);
+            break;
+        default:
+            printf("Вероятно, такой команды не существует\n");
+        }
+    }
+    deleteList(list);
+    printf("Работа с программой завершена. Память очищена\n");
+}
+
+int main()
+{
+    printMenu();
+    handleCommands();
+    return 0;
+}
