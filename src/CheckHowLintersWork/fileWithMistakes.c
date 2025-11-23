492f971af550f5129d2468bf29c725f698604c0e
Yana Kalsina <yaprogrammer18@gmail.com>
2025-11-23 12:15:49 +0300

    Changed file eith mistakes

--- src/CheckHowLintersWork/fileWithMistakes.c
+++ src/CheckHowLintersWork/fileWithMistakes.c
@@ -5,43 +5,6 @@
 
 int main(int argc, char* argv[])
 {
-    char dir[1024] = { 0 };
-    char cmd[1200] = { 0 };
-    char buff[1024] = { 0 };
-    FILE* fp = NULL;
-    size_t bytesRead = 0;
-    const char* homeDir = NULL;
-
-    if (argc == 2) {
-        snprintf(dir, sizeof(dir), "%s", argv[1]);
-    } else {
-        homeDir = getenv("HOME");
-        if (homeDir != NULL) {
-            snprintf(dir, sizeof(dir), "%s", homeDir);
-        } else {
-            snprintf(dir, sizeof(dir), "%s", "/");
-        }
-    }
-
-    snprintf(cmd, sizeof(cmd), "ls \"%s\"", dir);
-
-    fp = popen(cmd, "r");
-    if (fp == NULL) {
-        fputs("Failed to invoke command\n", stderr);
-        return -1;
-    }
-
-    while ((bytesRead = fread(buff, 1, sizeof(buff) - 1, fp)) > 0) {
-        buff[bytesRead] = '\0';
-        printf("%s", buff);
-    }
-
-    if (ferror(fp)) {
-        fputs("Error reading from command\n", stderr);
-        pclose(fp);
-        return -1;
-    }
-
-    pclose(fp);
+    printf("Now, I'm sure, that everything works as it supposed to..");
     return 0;
 }
