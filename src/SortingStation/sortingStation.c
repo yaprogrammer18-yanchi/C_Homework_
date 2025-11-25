b214c45121823e0d124077136902c8ea023185f8
Yana Kalsina <yaprogrammer18@gmail.com>
2025-11-25 19:51:40 +0300

    Fix all mistakes

--- src/SortingStation/sortingStation.c
+++ src/SortingStation/sortingStation.c
@@ -5,7 +5,7 @@
 #include <string.h>
 
 // функция для определения приоритета операнда +- это 1, * / это 2
-int getPreoritite(char operand)
+int getPriority(char operand)
 {
     if (operand == '+' || operand == '-') {
         return 1;
@@ -17,58 +17,64 @@ int getPreoritite(char operand)
 
 char* convertString(char* string, int len)
 {
-    struct Stack stack = newStack();
-    char* queue = malloc(101 * sizeof(char));
-    // не все значения будут идти в очередь, а надо, чтобы элементы в ней располагались один за другим, для этого пусть будет этот счетчик
-    int countForQueue = 0;
+    struct Stack* stack = newStack();
+    char* queue = malloc(1000 * sizeof(char));
+    int queueEndPtr = 0;
     int i = 0;
     while (i != len) {
         // если число
         if (isdigit(string[i])) {
-            queue[countForQueue] = string[i];
-            ++countForQueue;
+            queue[queueEndPtr] = string[i];
+            ++queueEndPtr;
+            queue[queueEndPtr] = ' ';
+            ++queueEndPtr;
         }
         // если открывающаяся скобка, добавляем ее в стек
         if (string[i] == '(') {
-            push(&stack, string[i]);
+            push(stack, string[i]);
         }
         // если закрывающаяся, то переберем все значения стека и разместим их в очереди, пока не встретится открывающая скобка
         else if (string[i] == ')') {
-            while ((stack.head != NULL) && (peek(&stack) != '(')) {
-                queue[countForQueue] = pop(&stack);
-                ++countForQueue;
+            while ((!isEmpty(stack)) && (peek(stack) != '(')) {
+                queue[queueEndPtr] = pop(stack);
+                ++queueEndPtr;
+                queue[queueEndPtr] = ' ';
+                ++queueEndPtr;
             }
-            if ((stack.head != NULL) && (peek(&stack) == '(')) {
-                pop(&stack);
-            } else if (stack.head == NULL) {
-                deleteStack(&stack);
+            if ((!isEmpty(stack)) && (peek(stack) == '(')) {
+                pop(stack);
+            } else if (!isEmpty(stack)) {
+                deleteStack(stack);
                 return "ERROR: you missed openning bracket symbol";
             }
         }
         // если операторы + - * /
         if ((string[i] == '+') || (string[i] == '-') || (string[i] == '*') || (string[i] == '/')) {
             // если оператор, но по приоритету такой же или меньший чем в стеке
-            while ((stack.head != NULL) && (getPreoritite(string[i]) <= getPreoritite(peek(&stack)))) {
-                queue[countForQueue] = pop(&stack);
-                ++countForQueue;
+            while ((!isEmpty(stack)) && (getPriority(string[i]) <= getPriority(peek(stack)))) {
+                queue[queueEndPtr] = pop(stack);
+                ++queueEndPtr;
+                queue[queueEndPtr] = ' ';
+                ++queueEndPtr;
             }
-            push(&stack, string[i]);
+            push(stack, string[i]);
         }
         ++i;
     }
-    if (stack.head != NULL) {
-        while (stack.head != NULL) {
-            if (peek(&stack) == '(') {
-                deleteStack(&stack);
-                return "ERROR: you missed closing bracket symbol";
-            }
-            queue[countForQueue] = pop(&stack);
-            ++countForQueue;
+    while (!isEmpty(stack)) {
+        if (peek(stack) == '(') {
+            deleteStack(stack);
+            return "ERROR: you missed closing bracket symbol";
         }
+        queue[queueEndPtr] = pop(stack);
+        ++queueEndPtr;
+        queue[queueEndPtr] = ' ';
+        ++queueEndPtr;
     }
-    deleteStack(&stack);
+
+    deleteStack(stack);
     // чтобы строка была корректной
-    queue[countForQueue] = '\0';
+    queue[queueEndPtr] = '\0';
     return queue;
 }
 
@@ -79,18 +85,16 @@ char* convertString(char* string, int len)
 
 int main()
 {
-    char* arifmeticExpression = malloc(101 * sizeof(char));
-    // 101 так как последний символ отводится для \0
-    printf("Введите выражение, не превышающее по длине 100 символов\n");
-    fgets(arifmeticExpression, 101, stdin);
+    char* arifmeticExpression = malloc(1001 * sizeof(char));
+    // 1001 так как последний символ отводится для \0
+    printf("Введите выражение, не превышающее по длине 1001 символов\n");
+    fgets(arifmeticExpression, 1001, stdin);
     char* queue = convertString(arifmeticExpression, strlen(arifmeticExpression));
-    for (unsigned i = 0; i <= strlen(queue); i++) {
-        if (i == 0) {
-            printf("%c", queue[i]);
-        } else if (i == strlen(queue)) {
-            printf("%c\n", queue[i]);
-        } else {
-            printf(" %c ", queue[i]);
+    int queueLength = strlen(queue);
+    for (unsigned i = 0; i <= queueLength; i++) {
+        printf("%c", queue[i]);
+        if (i == strlen(queue)) {
+            printf("\n");
         }
     }
     free(arifmeticExpression);
