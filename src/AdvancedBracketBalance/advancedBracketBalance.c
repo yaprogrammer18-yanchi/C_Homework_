d0aa365c9a09c86af971cb6224f8231af2e5febf
Yana Kalsina <yaprogrammer18@gmail.com>
2025-11-25 18:52:22 +0300

    Fix advanced braket balance task

--- src/AdvancedBracketBalance/advancedBracketBalance.c
+++ src/AdvancedBracketBalance/advancedBracketBalance.c
@@ -2,75 +2,53 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-bool checkBrackets(int counter, char *stringWithBrackets) {
-  if (counter == 0) {
-    return false;
-  }
-  Stack *stack = newStack();
-  char tmp = ' ';
-  for (int i = 0; i < counter; i++) {
-    tmp = stringWithBrackets[i];
-    // проверка что стек не пустой
-    if ((tmp == '(') || (tmp == '{') || (tmp == '[')) {
-      push(stack, tmp);
-    }
-    // если встретилась закрывающаяся и стек не пустой
-    else if ((tmp == ')' || tmp == '}' || tmp == ']') && (!isEmpty(stack))) {
-      char previousBracket = pop(stack);
-      // проверка скобок одного типа через номера из таблицы ASCII
+bool checkBrackets(char* stringWithBrackets)
+{
+    bool isBalanced = true;
+    Stack* stack = newStack();
+    char tmp = ' ';
+    int length = strlen(stringWithBrackets);
+    for (int i = 0; i < length; i++) {
+        tmp = stringWithBrackets[i];
+        if ((tmp == '(') || (tmp == '{') || (tmp == '[')) {
+            push(stack, tmp);
+        }
+        // если встретилась закрывающаяся и стек не пустой
+        else if ((tmp == ')' || tmp == '}' || tmp == ']') && (!isEmpty(stack))) {
+            char previousBracket = pop(stack);
+            if ((previousBracket == '(' && tmp == ')') || (previousBracket == '[' && tmp == ']') || (previousBracket == '{' && tmp == '}')) {
+                continue;
 
-      if ((previousBracket + 1 == tmp) || (previousBracket + 2 == tmp)) {
-        continue;
-      } else {
-        deleteStack(stack);
-        return false;
-      }
+            } else {
+                isBalanced = false;
+                break;
+            }
+        }
+        // закрывающаяся и пустой стек
+        else if ((tmp == ')' || tmp == '}' || tmp == ']') && (isEmpty(stack))) {
+            isBalanced = false;
+        }
     }
-    // закрывающаяся и пустой стек
-    else if ((tmp == ')' || tmp == '}' || tmp == ']') && (isEmpty(stack))) {
-      deleteStack(stack);
-      return false;
+    if (!isEmpty(stack)) {
+        isBalanced = false;
     }
-  }
-  if (isEmpty(stack)) {
     deleteStack(stack);
-    return true;
-  }
-  deleteStack(stack);
-  return false;
+    return isBalanced;
 }
 
-int main() {
-  // подразумеватся, что пользователь может ввести не только скобки и в кол-ве
-  // символов он должен указать всё кол-во символов включая скобки и не скобки.
-  int len = 0;
-  printf("Введите, сколько символов Вы хотите ввести, закончите ввод, нажав "
-         "enter\n");
-  printf("На следующей строке введите скобочную последовательность\n");
-  ;
-  scanf("%d", &len);
-  // отловим этот '\n'
-  getchar();
-  char *stringWithBrackets = malloc(len * sizeof(char));
-  int counter = 0;
-  char tmp = ' ';
-  for (int i = 0; i < len; i++) {
-    scanf("%c", &tmp);
-
-    if ((tmp == 40) || (tmp == 41) || (tmp == 91) || (tmp == 93) ||
-        (tmp == 123) || (tmp == 125)) {
-      stringWithBrackets[counter] = tmp;
-      ++counter;
+int main()
+{
+    char* stringWithBrackets = malloc(1000 * sizeof(char));
+    printf("Введите скобочную последовательность: ");
+    scanf("%s", stringWithBrackets);
+    bool isCorrect = checkBrackets(stringWithBrackets);
+    if (isCorrect) {
+        printf("Скобочная последовательность правильная\n");
+    } else {
+        printf("Скобочная последовательность неправильная\n");
     }
-  }
-  bool isCorrect = checkBrackets(counter, stringWithBrackets);
-  if (isCorrect) {
-    printf("Скобочная последовательность правильная\n");
-  } else {
-    printf("Скобочная последовательность неправильная или не было введено ни "
-           "одной скобки\n");
-  }
-  free(stringWithBrackets);
-  return 0;
+    free(stringWithBrackets);
+    return 0;
 }
