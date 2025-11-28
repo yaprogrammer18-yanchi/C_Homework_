281c073e9dcd61c1d1d00b4701c83ffa4816860b
Yana Kalsina <yaprogrammer18@gmail.com>
2025-11-28 21:08:07 +0300

    Formatted files

--- src/optimalSortingDir/main.c
+++ src/optimalSortingDir/main.c
@@ -2,28 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-  int *arrayWithNumbers = calloc(100, sizeof(int));
-  int length = 0;
-  char nextSymbol = 0;
-  while (nextSymbol != '\n') {
-    int inputNumber = 0;
-    int input = scanf("%d", &inputNumber);
-    // scanf успешно прочитал введенное число
-    if (input == 1) {
-      // добавляем число в список
-      arrayWithNumbers[length++] = inputNumber;
-    } else {
-      return 1;
+int main()
+{
+    int* arrayWithNumbers = calloc(100, sizeof(int));
+    int length = 0;
+    char nextSymbol = 0;
+    while (nextSymbol != '\n') {
+        int inputNumber = 0;
+        int input = scanf("%d", &inputNumber);
+        // scanf успешно прочитал введенное число
+        if (input == 1) {
+            // добавляем число в список
+            arrayWithNumbers[length++] = inputNumber;
+        } else {
+            return 1;
+        }
+        // читаем символ, который был введен дальше
+        nextSymbol = getchar();
+        if (nextSymbol == '\n') {
+            break;
+        }
     }
-    // читаем символ, который был введен дальше
-    nextSymbol = getchar();
-    if (nextSymbol == '\n') {
-      break;
-    }
-  }
-  printf("%d\n", sort(arrayWithNumbers, length));
-  return 0;
+    printf("%d\n", sort(arrayWithNumbers, length));
+    return 0;
 }
 
 // пример: ввод 4 3 2 1
--- src/optimalSortingDir/sort.c
+++ src/optimalSortingDir/sort.c
@@ -2,27 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
-int sort(int *array, int length) {
-  // c помощью этого списка будем считать элементы, изменившие свою позицию
-  int *unsortedArray = calloc(length, sizeof(int));
-  memcpy(unsortedArray, array, sizeof(int) * length);
-  // сортировка пузырьком
-  for (int i = 0; i < length; i++) {
-    for (int j = 0; j < length - 1 - i; j++) {
-      if (array[j] > array[j + 1]) {
-        int tmp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = tmp;
-      }
+int sort(int* array, int length)
+{
+    // c помощью этого списка будем считать элементы, изменившие свою позицию
+    int* unsortedArray = calloc(length, sizeof(int));
+    memcpy(unsortedArray, array, sizeof(int) * length);
+    // сортировка пузырьком
+    for (int i = 0; i < length; i++) {
+        for (int j = 0; j < length - 1 - i; j++) {
+            if (array[j] > array[j + 1]) {
+                int tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+            }
+        }
     }
-  }
-  int count = 0;
-  // смотрим какие элементы изменили свою позицию
-  for (int k = 0; k < length; k++) {
-    if (array[k] != unsortedArray[k]) {
-      ++count;
+    int count = 0;
+    // смотрим какие элементы изменили свою позицию
+    for (int k = 0; k < length; k++) {
+        if (array[k] != unsortedArray[k]) {
+            ++count;
+        }
     }
-  }
-  free(unsortedArray);
-  return count;
+    free(unsortedArray);
+    return count;
 }
